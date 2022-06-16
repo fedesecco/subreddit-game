45f3e18cf06695c0221f96801e44a85c4f8f10fc
Federico Secco <federico.secco7@gmail.com>
2022-06-16 15:05:55 +0200

    connecting to backend

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import { Button, Alert, Container } from 'react-bootstrap';
 import './App.scss';
 
-const testVariable = "TEST";
-const App = () => {
+function App() {
+  const serverURL = 'https://lit-garden-12978.herokuapp.com/';
+  const [robots, setRobots] = useState([])
+
+  /*   useEffect(() => {
+      fetch(serverURL)
+        .then(response => response.json())
+        .then(users => { setRobots(users) });
+    }, []) */
+
   return (
     <div>
       <Header />
@@ -15,6 +23,18 @@ const App = () => {
     </div>
   );
 }
+
+/* const App = () => {
+  return (
+    <div>
+      <Header />
+      <Container className="mt-5 pb-3"></Container>
+      <Alert className="tc" key="primary" variant="primary">
+        Test allerta
+      </Alert>
+    </div>
+  );
+} */
 
 
 export default App;
