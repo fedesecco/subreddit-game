9e4cda1f5f6f082e713b2bbfa01d06e54fe9d445
Federico Secco <federico.secco7@gmail.com>
2022-09-07 23:53:43 +0200

    Delete src directory

--- src/testUsers.js
+++
@@ -1,9 +0,0 @@
-export const users = [
-  {
-    id: 1,
-    username: 'Bret',
-    email: 'Sincere@april.biz',
-    preferredGame: 1,
-    joined: 'Tue Jun 21 2022 23:48:02 GMT+0200 (Central European Summer Time)'
-  },
-];
