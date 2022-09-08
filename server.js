d4d1191650d901939435034c334518c1b9ac290e
Federico Secco <federico.secco7@gmail.com>
2022-09-08 12:13:39 +0200

    continuing 1

--- server.js
+++ server.js
@@ -2,15 +2,21 @@ import express from "express"
 const app = express()
 import dotenv from "dotenv"
 dotenv.config()
+//db and auth
 import connectDB from "./db/connect.js"
+//routers
+import authRouter from "./routes/authRoutes.js"
 //middlewares
 import erroreHandlerMiddleWare from "./middleware/error-handler.js"
 import notFoundMiddleWare from "./middleware/not-found.js"
 
+app.use(express.json())
 
 app.get("/", (req,res)=>{
     res.send(`Welcome on port ${port}!`)
 })
+
+app.use("api/v1/auth", authRouter)
 
 app.use(notFoundMiddleWare)
 app.use(erroreHandlerMiddleWare)
